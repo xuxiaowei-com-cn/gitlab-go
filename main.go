b14e3fe4c4e217e3ba9ea49ce06179e47282af75
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-10-01 22:43:51 +0800

    :sparkles: 新增 --version

--- main.go
+++ main.go
@@ -8,10 +8,13 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+const version = "v0.0.1-snapshot" // 版本号
+
 func main() {
 	app := &cli.App{
-		Name:  "boom",
-		Usage: "make an explosive entrance",
+		Name:    "boom",
+		Version: version,
+		Usage:   "make an explosive entrance",
 		Action: func(*cli.Context) error {
 			fmt.Println("boom! I say!")
 			return nil

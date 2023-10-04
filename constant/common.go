8a45d763aba17711d757f66b04325d7988c4ea28
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-10-05 01:44:18 +0800

    :construction: 参数名不含大写字母

--- constant/common.go
+++ constant/common.go
@@ -2,11 +2,11 @@ package constant
 
 const (
 	Token          = "token"
-	BaseUrl        = "baseUrl"
+	BaseUrl        = "base-url"
 	Request        = "request"
 	BaseUrlDefault = "https://gitlab.com/api/v4"
 	Id             = "id"
-	JobId          = "jobId"
+	JobId          = "job-id"
 	Sort           = "sort"
 	SortDefault    = "desc"
 )

c1ae9621937977b27d887ccec77f652c81eefe07
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-10-04 22:29:47 +0800

    :white_check_mark: 添加 projects 测试

--- main_test.go
+++ main_test.go
@@ -1,11 +1,41 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/xuxiaowei-com-cn/git-go/buildinfo"
+	"os"
+	"strings"
 	"testing"
 )
 
 func Test_CommitSha(t *testing.T) {
 	fmt.Println(buildinfo.CommitSha())
+}
+
+func Test_Projects(t *testing.T) {
+	var buf bytes.Buffer
+
+	// 不带参数的测试输出
+	os.Args = []string{"cmd"}
+	main()
+	if got := buf.String(); !strings.Contains(got, "") {
+		t.Errorf("异常信息：\n%s", got)
+	}
+
+	buf.Reset()
+
+	// 使用版本参数测试输出
+	os.Args = []string{"cmd", "projects"}
+	main()
+	if got := buf.String(); !strings.Contains(got, "") {
+		t.Errorf("异常信息：\n%s", got)
+	}
+
+	// 使用版本参数测试输出
+	os.Args = []string{"cmd", "p"}
+	main()
+	if got := buf.String(); !strings.Contains(got, "") {
+		t.Errorf("异常信息：\n%s", got)
+	}
 }
